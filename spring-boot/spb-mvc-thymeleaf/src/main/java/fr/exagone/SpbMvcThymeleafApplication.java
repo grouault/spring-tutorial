8620fcbcb02923fd82155c6922592cb2c478f315
grouault <gildasrouault@gmail.com>
2020-05-14 00:11:10 +0200

    Ajout Spring-Boot

---
+++ spring-boot/spb-mvc-thymeleaf/src/main/java/fr/exagone/SpbMvcThymeleafApplication.java
@@ -0,0 +1,13 @@
+package fr.exagone;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpbMvcThymeleafApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpbMvcThymeleafApplication.class, args);
+	}
+
+}
---
+++ spring-boot/spb-mvc-thymeleaf/src/test/java/fr/exagone/spbmvcthymeleaf/SpbMvcThymeleafApplicationTests.java
@@ -0,0 +1,13 @@
+package fr.exagone.spbmvcthymeleaf;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpbMvcThymeleafApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
